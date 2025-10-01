51d170dc6bee5d183aac16437232e8bcce056e2b
Wolfgang Grieskamp <wg@aptoslabs.com>
2025-10-01 10:17:53 -0700

    [vm] Add mechanism to trace execution via env vars (#17733)
    * [vm] Add mechanism to trace execution via env vars
    
    This adds a mechanism to trace execution using an env var `MOVE_TRACE_EXEC`. This env var can specify a list of conditions to trace, for example, `MOVE_TRACE_EXEC=abort(12),abort(44)` will trace exactly those aborts, printing the (Move) stack trace. Printing is similar as with `std::debug::print_stack_trace` (which is only available in unit tests.) The mechanism is designed to be extensible for future tracing categories and for local debugging.
    
    Manually testing verifying output of
    
    ```
    MOVE_TRACE_EXEC=vm_error cargo test -p e2e-move-tests code_publishing_upgrade_fail_immutable -- --nocapture
    ```
    
    * Addressing reviewer comments
    
    Downstreamed-from: 46d871fa1feb61ffafb73353a0755e8cc3aaed9d

--- third_party/move/move-vm/types/src/values/values_impl.rs
+++ third_party/move/move-vm/types/src/values/values_impl.rs
@@ -2157,6 +2157,10 @@ impl Value {
     {
         VMValueCast::cast(self)
     }
+
+    pub fn is_invalid(&self) -> bool {
+        matches!(self.0, ValueImpl::Invalid)
+    }
 }
 
 impl VMValueCast<u8> for IntegerValue {
@@ -3809,9 +3813,16 @@ pub mod debug {
         }
     }
 
-    pub fn print_locals<B: Write>(buf: &mut B, locals: &Locals) -> PartialVMResult<()> {
+    pub fn print_locals<B: Write>(
+        buf: &mut B,
+        locals: &Locals,
+        compact: bool,
+    ) -> PartialVMResult<()> {
         // REVIEW: The number of spaces in the indent is currently hard coded.
         for (idx, val) in locals.0.borrow().iter().enumerate() {
+            if compact && matches!(val, ValueImpl::Invalid) {
+                continue;
+            }
             debug_write!(buf, "            [{}] ", idx)?;
             print_value_impl(buf, val)?;
             debug_writeln!(buf)?;

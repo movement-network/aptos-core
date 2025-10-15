d8b3e18d772cca00843ce83db32d1e9a9a72cbed
Andy Golay <andygolay@gmail.com>
2025-10-15 07:17:07 -0400

    fix: update transaction explorer links

--- crates/aptos/src/common/utils.rs
+++ crates/aptos/src/common/utils.rs
@@ -589,13 +589,13 @@ pub fn explorer_transaction_link(
     // For now, default to what the browser is already on, though the link could be wrong
     if let Some(network) = network {
         format!(
-            "https://explorer.aptoslabs.com/txn/{}?network={}",
+            "https://explorer.movementlabs.xyz/txn/{}?network={}",
             hash.to_hex_literal(),
             network
         )
     } else {
         format!(
-            "https://explorer.aptoslabs.com/txn/{}",
+            "https://explorer.movementlabs.xyz/txn/{}",
             hash.to_hex_literal()
         )
     }

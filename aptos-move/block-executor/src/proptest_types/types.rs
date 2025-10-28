af121eb83c655c7a390da2fdb5c3e4f46e7e9700
Victor Gao <10379359+vgao1996@users.noreply.github.com>
2025-10-28 09:47:35 -0700

    [Move VM] optimize ReentrancyChecker (alternative approach) (#17877)
    * [Move VM] optimize ReentrancyChecker
    
    * [vm] StructIdentifier::new
    
    Downstreamed-from: 452b44a1326aa46c014ea2aa034fe7268cb59035

--- aptos-move/block-executor/src/proptest_types/types.rs
+++ aptos-move/block-executor/src/proptest_types/types.rs
@@ -25,7 +25,7 @@ use move_core_types::{
     language_storage::ModuleId,
 };
 use move_vm_runtime::Module;
-use move_vm_types::delayed_values::delayed_field_id::{DelayedFieldID, ExtractUniqueIndex};
+use move_vm_types::module_id_interner::InternedModuleIdPool;
 use proptest::{arbitrary::Arbitrary, collection::vec, prelude::*, proptest, sample::Index};
 use proptest_derive::Arbitrary;
 use std::{
@@ -746,6 +746,7 @@ impl<V: Into<Vec<u8>> + Arbitrary + Clone + Debug + Eq + Sync + Send> Transactio
         E: Send + Sync + Debug + Clone + TransactionEvent,
     >(
         self,
+        module_id_pool: &InternedModuleIdPool,
         universe: &[K],
     ) -> MockTransaction<KeyType<K>, E> {
         let universe_len = universe.len();
@@ -761,7 +762,7 @@ impl<V: Into<Vec<u8>> + Arbitrary + Clone + Debug + Eq + Sync + Send> Transactio
 
             // Serialize a module and store it in bytes so deserialization can succeed.
             let mut serialized_bytes = vec![];
-            Module::new_for_test(module_id.clone())
+            Module::new_for_test(module_id_pool, module_id.clone())
                 .serialize(&mut serialized_bytes)
                 .expect("Failed to serialize compiled module");
             value.bytes = Some(serialized_bytes.into());

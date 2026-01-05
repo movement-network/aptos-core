f04331449feed87e53e23058910d8867a6430eaf
apenzk <dr.andreas.penzkofer@gmail.com>
2026-01-05 17:12:38 +0100

    wrong headers

--- api/src/tests/verification_test.rs
+++ api/src/tests/verification_test.rs
@@ -1,6 +1,3 @@
-// Copyright © Aptos Foundation
-// SPDX-License-Identifier: Apache-2.0
-
 //! Unit tests for bytecode verification API endpoint.
 
 use super::new_test_context_with_config;
--- api/src/verification.rs
+++ api/src/verification.rs
@@ -1,6 +1,3 @@
-// Copyright © Aptos Foundation
-// SPDX-License-Identifier: Apache-2.0
-
 //! On-demand bytecode verification for PackageRegistry source code.
 //!
 //! Verifies that source code stored in PackageRegistry compiles to the same

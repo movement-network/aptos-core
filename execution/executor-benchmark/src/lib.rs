d3316ced29742c2fb8f19f879e497d8b7d4bfeb2
Sean Young <sean@mess.org>
2026-07-02 15:13:26 +0100

    Fix test_compare_vm_and_vm_uncoordinated test

--- execution/executor-benchmark/src/lib.rs
+++ execution/executor-benchmark/src/lib.rs
@@ -712,7 +712,7 @@ mod tests {
         fa_features.enable(FeatureFlag::NEW_ACCOUNTS_DEFAULT_TO_FA_APT_STORE);
         fa_features.enable(FeatureFlag::OPERATIONS_DEFAULT_TO_FA_APT_STORE);
         fa_features.enable(FeatureFlag::NEW_ACCOUNTS_DEFAULT_TO_FA_STORE);
-        fa_features.disable(FeatureFlag::CONCURRENT_FUNGIBLE_BALANCE);
+        //fa_features.disable(FeatureFlag::CONCURRENT_FUNGIBLE_BALANCE);
 
         test_compare_prod_and_another::<E>(values_match, fa_features.clone(), |address| {
             aptos_stdlib::aptos_account_fungible_transfer_only(address, 1000)
